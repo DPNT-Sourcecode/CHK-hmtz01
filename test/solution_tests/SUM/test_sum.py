c702056e719cf70511c010d967673f893ddf6aa5
sbx_user1051 <sbx_user1051@169.254.133.85>
2024-06-27 15:56:50 +0000

    Thu Jun 27 15:56:50 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,10 @@
 from solutions.SUM import sum_solution
+from hypothesis import given, strategies as st
 
+def test_sum():
+    assert sum_solution.compute(1, 2) == 3
 
-class TestSum():
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+@given(st.integer(), st.integer())
+def test_sum_ints_is_int(x, y):
+    assert isinstance(sum_solution.compute(x, y), int)
+
