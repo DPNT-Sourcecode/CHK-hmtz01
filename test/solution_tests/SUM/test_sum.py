dd8615b092fb9b1aeca42eb4bdcbcf5b309f9ff7
sbx_user1051 <sbx_user1051@169.254.133.85>
2024-06-27 15:59:50 +0000

    Thu Jun 27 15:59:50 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,7 +4,8 @@ from hypothesis import given, strategies as st
 def test_sum():
     assert sum_solution.compute(1, 2) == 3
 
-@given(st.integer(), st.integer())
+@given(st.integers(min_value=0, max_value=100), st.integers(min_value=0, max_value=100))
 def test_sum_ints_is_int(x, y):
     assert isinstance(sum_solution.compute(x, y), int)
+
 
