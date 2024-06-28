8d1b8e085c4b3a1e848d38c14cc78274123abdde
sbx_user1051 <sbx_user1051@169.254.41.9>
2024-06-28 14:16:34 +0000

    Fri Jun 28 14:16:34 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -41,6 +41,16 @@ def price_step(subtotal, basket):
         return (subtotal + 130, {**basket, "A": a - 3})
     if b >= 2:
         return (subtotal + 45, {**basket, "B": b - 2})
+    if h >= 10:
+        return (subtotal + 80, {**basket, "H": h - 10})
+    if h >= 5:
+        return (subtotal + 45, {**basket, "H": h - 5})
+    if k >= 2:
+        return (subtotal + 150, {**basket, "K": k - 2})
+    if n >= 3 and m >= 1:
+        return (subtotal + 120, {**basket, "N": n - 3, "M": m - 1})
+    if x >= 1:
+        return (subtotal + 10, {**basket, "F": f - 1})
     if a >= 1:
         return (subtotal + 50, {**basket, "A": a - 1})
     if b >= 1:
@@ -53,9 +63,50 @@ def price_step(subtotal, basket):
         return (subtotal + 40, {**basket, "E": e - 1})
     if f >= 1:
         return (subtotal + 10, {**basket, "F": f - 1})
+    if g >= 1:
+        return (subtotal + 20, {**basket, "G": g - 1})
+    if h >= 1:
+        return (subtotal + 10, {**basket, "H": h - 1})
+    if i >= 1:
+        return (subtotal + 35, {**basket, "I": i - 1})
+    if j >= 1:
+        return (subtotal + 60, {**basket, "J": j - 1})
+    if k >= 1:
+        return (subtotal + 80, {**basket, "K": k - 1})
+    if l >= 1:
+        return (subtotal + 90, {**basket, "L": l - 1})
+    if m >= 1:
+        return (subtotal + 15, {**basket, "M": m - 1})
+    if n >= 1:
+        return (subtotal + 40, {**basket, "N": n - 1})
+    if o >= 1:
+        return (subtotal + 10, {**basket, "O": o - 1})
+    if p >= 1:
+        return (subtotal + 50, {**basket, "P": p - 1})
+    if q >= 1:
+        return (subtotal + 30, {**basket, "Q": q - 1})
+    if r >= 1:
+        return (subtotal + 50, {**basket, "R": r - 1})
+    if s >= 1:
+        return (subtotal + 30, {**basket, "S": s - 1})
+    if t >= 1:
+        return (subtotal + 20, {**basket, "T": t - 1})
+    if u >= 1:
+        return (subtotal + 40, {**basket, "U": u - 1})
+    if v >= 1:
+        return (subtotal + 50, {**basket, "V": v - 1})
+    if w >= 1:
+        return (subtotal + 20, {**basket, "W": w - 1})
+    if x >= 1:
+        return (subtotal + 90, {**basket, "X": x - 1})
+    if y >= 1:
+        return (subtotal + 10, {**basket, "Y": y - 1})
+    if z >= 1:
+        return (subtotal + 50, {**basket, "Z": z - 1})
 
 
     if all(v == 0 for v in basket.values()):
         return subtotal, None
     else:
         return -1, None
+
