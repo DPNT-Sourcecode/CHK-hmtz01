ec4db2f150eff0b00f7d8d048d5ad2a698ecbd0c
sbx_user1051 <sbx_user1051@169.254.41.9>
2024-06-28 13:14:43 +0000

    Fri Jun 28 13:14:43 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,11 @@
-
+from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
+
+def counter(skus):
+    return Counter(skus)
+
