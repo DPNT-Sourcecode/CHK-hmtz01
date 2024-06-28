1ab5561dd2019fc86e760a63957ef0f17230b7fc
sbx_user1051 <sbx_user1051@169.254.41.9>
2024-06-28 14:04:32 +0000

    Fri Jun 28 14:04:32 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -29,11 +29,12 @@ def price_step(subtotal, basket):
     c = basket.get("C", 0)
     d = basket.get("D", 0)
     e = basket.get("E", 0)
+    f = basket.get("F", 0)
 
     if e >= 2 and b >= 1:
         return (subtotal + 80, {**basket, "E": e - 2, "B": b - 1})
     if f >= 3:
-        return (subtotal + 30, {**basket, "F": f - 3})
+        return (subtotal + 20, {**basket, "F": f - 3})
     if a >= 5:
         return (subtotal + 200, {**basket, "A": a - 5})
     if a >= 3:
@@ -50,12 +51,15 @@ def price_step(subtotal, basket):
         return (subtotal + 15, {**basket, "D": d - 1})
     if e >= 1:
         return (subtotal + 40, {**basket, "E": e - 1})
+    if f >= 1:
+        return (subtotal + 10, {**basket, "F": f - 1})
 
 
     if all(v == 0 for v in basket.values()):
         return subtotal, None
     else:
         return -1, None
+
 
 
 
